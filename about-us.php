da9e256aa5aaa43f656963b0f8244d81faf97486
ingruiz2025 <ingruiz2025@gmail.com>
2025-09-24 04:50:57 +0200

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,50 @@
+<?php include('shared/_header.php');?>
+
+<body>
+    <main>
+        <div class="big-wrapper light">
+            <img src="./images/shape.png" alt="" class="shape" />
+
+            
+     <?php include('shared/_navbar.php'); ?>
+
+
+
+            <div class="container mt-5">
+
+
+    
+                <div class="card border-0 shadow about-card">
+                    <div class="card-body mt-5 mb-2 mx-3 ">
+                        <h3 class="card-title">
+                        <span class="text-primary icon-hover"><i class="fa-solid fa-paperclip"></i></span>    
+                        About us</h3>
+                        <p class="card-text fs-5 text-justify">
+                        Somos un equipo comprometido con la creación de aplicaciones que lleven como protagonista al nuestros usuarios y en especial a aquellos con  capacidades diferentes para que su experiencia sea la mejor.
+                    </div>
+                    <div class="mb-4" style="width: 100%;  display: flex; align-items: center; justify-content: center;">
+                    <img style="width: 400px;"  src="./images/about.png" class="card-img-bottom" alt="...">
+                    </div>
+                </div>
+
+
+                
+            </div>
+
+
+
+
+
+
+
+
+        </div>
+
+
+    </main>
+
+
+
+
+  
+    <?php include('shared/_footer.php'); ?>
